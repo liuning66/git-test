5cce33ab61cde090e9405e42d9c63b7eec9dff7a
liuning <1327023076@qq.com>
2022-03-11 15:13:12 +0800

    otheq:wqfil

---
+++ pages/test2/test2.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+			这是test界面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
